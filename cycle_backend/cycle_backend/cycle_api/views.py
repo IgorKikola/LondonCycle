6596e3868bb3c8c3bc46a806b97a9c39bc5ea159
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2022-02-14 17:15:13 +0200

    Add view to get all BikePoint locations from TFL API

--- cycle_backend/cycle_backend/cycle_api/views.py
+++ cycle_backend/cycle_backend/cycle_api/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
 from cycle_backend.cycle_api.serializers import UserSerializer
+from cycle_backend.cycle_api.models import Place
+from django.http import HttpResponse
+import requests
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +14,15 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+def get_all_bikepoints(request):
+    Place.objects.all().delete()
+    response = requests.get('https://api.tfl.gov.uk/BikePoint/')
+    bikepoints = response.json()
+    for bikepoint in bikepoints:
+        place = Place.create(bikepoint['id'],
+                             bikepoint['commonName'],
+                             bikepoint['lat'],
+                             bikepoint['lon'])
+        place.save()
+    return HttpResponse("Bikes")
