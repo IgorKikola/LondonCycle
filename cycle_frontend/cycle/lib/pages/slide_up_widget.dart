0e5e3a692e4cb9b32bf6413fa1b68ab4b48acb9e
abbenayanjey <abbej27@gmail.com>
2022-02-17 23:53:28 +0000

    Add landmarks and nearest bikepoints to sliding panel

--- cycle_frontend/cycle/lib/pages/slide_up_widget.dart
+++ cycle_frontend/cycle/lib/pages/slide_up_widget.dart
@@ -13,6 +13,8 @@ class SlideUpWidget extends StatelessWidget {
           buildSlidingHandle(),
           SizedBox(height: 10),
           buildSearchBar(),
+          SizedBox(height: 10),
+          buildWidgetGrid(),
         ],
       );
 
@@ -89,4 +91,289 @@ class SlideUpWidget extends StatelessWidget {
           ),
         ],
       )));
+
+  Widget buildWidgetGrid() => Container(
+    padding: EdgeInsets.all(1.0),
+    alignment: Alignment(-1.0, 0.0),
+    // constraints: BoxConstraints.tightForFinite(height: 400, width: 200),
+    height: 800,
+    width: 800,
+    child: GridView.count(
+      padding: EdgeInsets.all(1.0),
+      crossAxisCount: 2,
+      childAspectRatio: 0.5,
+      children: [
+        Center(
+          widthFactor: 300,
+          heightFactor: 400,
+          child: Container(
+              padding: EdgeInsets.all(1.0),
+              alignment: Alignment(1.0, 0.0),
+              child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(1),
+                        child: Container(
+                          padding: EdgeInsets.all(20.0),
+                          height: 230,
+                          width: 190,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('Landmarks:',
+                                        style: GoogleFonts.lato(
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white,
+                                            fontSize: 18)),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Big Ben',
+                                        style: GoogleFonts.lato(
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Icon(Icons.star, color: Colors.yellow),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Tower Bridge',
+                                        style: GoogleFonts.lato(
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Icon(Icons.star,
+                                        color: Colors.black.withOpacity(0.2)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('London Eye',
+                                        style: GoogleFonts.lato(
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Icon(Icons.star, color: Colors.yellow),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('The Shard',
+                                        style: GoogleFonts.lato(
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Icon(Icons.star,
+                                        color: Colors.black.withOpacity(0.2)),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))),
+        ),
+        Center(
+          widthFactor: 300,
+          heightFactor: 400,
+          child: Container(
+              padding: EdgeInsets.all(1.0),
+              alignment: Alignment(-1.0, 0.0),
+              child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Container(
+                          padding: EdgeInsets.all(20.0),
+                          height: 230,
+                          width: 190,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('Nearest Collection Points:',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 13,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Horseferry Road',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Text('0.5 miles away',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Westminister Pier',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Text('1.5 miles away',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Vauxhall Bridge',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Text('2 miles away',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                height: 30,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Milbank Tower',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                    Text('2.5 miles away',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontStyle: FontStyle.normal,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))),
+        ),
+      ],
+    ),
+  );
 }
