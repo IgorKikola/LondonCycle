9ad1f2a7c41c89dce82f14967697f062f5eef937
Igor Jasutowicz <k20069609@kcl.ac.uk>
2022-02-22 02:37:41 +0000

    Add backend functionality to retrieve all landmarks from CSV

--- cycle_backend/cycle_backend/cycle_api/views.py
+++ cycle_backend/cycle_backend/cycle_api/views.py
@@ -5,6 +5,7 @@ from cycle_backend.cycle_api.serializers import UserSerializer, PlaceSerializer
 from cycle_backend.cycle_api.models import Place
 from django.http import HttpResponse
 import requests
+import csv
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -33,3 +34,14 @@ def get_all_bikepoints(request):
                              bikepoint['lon'])
         place.save()
     return HttpResponse("Bikes")
+
+def get_all_landmarks(request):
+    with open('landmarks.csv', 'r') as file:
+        reader = csv.reader(file)
+        for landmark in reader:
+            place = Place.create(landmark[0],
+                                 landmark[1],
+                                 landmark[2],
+                                 landmark[3])
+            place.save()
+    return HttpResponse("Landmarks")
--- cycle_backend/cycle_backend/urls.py
+++ cycle_backend/cycle_backend/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('bikes/', views.get_all_bikepoints, name = 'bikes'),
+    path('landmarks/', views.get_all_landmarks, name = 'landmarks'),
 ]
