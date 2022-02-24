da300842a720496e7584bf658476e8d79dc448a2
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2022-02-24 18:25:24 +0200

    Add separate viewsets for bikepoints and landmarks

--- cycle_backend/cycle_backend/cycle_api/views.py
+++ cycle_backend/cycle_backend/cycle_api/views.py
@@ -19,3 +19,17 @@ class PlaceViewSet(viewsets.ModelViewSet):
     """
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
+
+class BikePointViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows bikepoints to be viewed or edited.
+    """
+    queryset = Place.objects.filter(id__startswith='BikePoints')
+    serializer_class = PlaceSerializer
+
+class LandmarkViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows landmarks to be viewed or edited.
+    """
+    queryset = Place.objects.filter(id__startswith='Landmark')
+    serializer_class = PlaceSerializer
