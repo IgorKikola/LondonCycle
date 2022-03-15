3c5e88c789247eb5ec2847d9af21fe424f01daa9
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2022-03-15 13:48:24 +0200

    Fix endpoint urls being incorrectly listed in the api root

--- cycle_backend/cycle_backend/urls.py
+++ cycle_backend/cycle_backend/urls.py
@@ -5,9 +5,9 @@ from cycle_backend.cycle_api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'places', views.PlaceViewSet)
-router.register(r'bikepoints', views.BikePointViewSet)
-router.register(r'landmarks', views.LandmarkViewSet)
+router.register(r'places', views.PlaceViewSet, basename='places')
+router.register(r'bikepoints', views.BikePointViewSet, basename='bikepoints')
+router.register(r'landmarks', views.LandmarkViewSet, basename='landmarks')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
