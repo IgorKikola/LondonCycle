b0080107ae641cfd6859fe0fda2fdd238bf6c37f
Igor Jasutowicz <k20069609@kcl.ac.uk>
2022-03-21 17:10:11 +0000

    Add view function for single stop routing and multiple stop routing

--- cycle_backend/cycle_backend/cycle_api/views.py
+++ cycle_backend/cycle_backend/cycle_api/views.py
@@ -3,6 +3,28 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from cycle_backend.cycle_api.serializers import UserSerializer, PlaceSerializer
 from cycle_backend.cycle_api.models import Place
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
+
+@api_view()
+def get_route(request, fromPlace, toPlace):
+    return Response(requests.get(f'https://api.tfl.gov.uk/Journey/JourneyResults/{fromPlace}/to/{toPlace}?/cyclePreference=CycleHire'))
+
+@api_view()
+def get_route_single_stop(request, fromPlace, firstStop, toPlace):
+    return Response(requests.get(f'https://api.tfl.gov.uk/Journey/JourneyResults/{fromPlace}/to/{toPlace}?via={firstStop}&cyclePreference=CycleHire'))
+
+@api_view()
+def get_route_multiple_stop(request, fromPlace, listStops, toPlace):
+    i = 0
+    return Response(requests.get(f'https://api.tfl.gov.uk/Journey/JourneyResults/{fromPlace}/to/{listStops[i]}?/cyclePreference=CycleHire'))
+    while i+1 <= len(listStops):
+        currentStop= listStops[i]
+        nextStop= listStops[i+i]
+        return Response(requests.get(f'https://api.tfl.gov.uk/Journey/JourneyResults/{currentStop}/to/{nextStop}?/cyclePreference=CycleHire'))
+        i+=1
+    return Response(requests.get(f'https://api.tfl.gov.uk/Journey/JourneyResults/{nextStop}/to/{toPlace}?/cyclePreference=CycleHire'))
 
 
 class UserViewSet(viewsets.ModelViewSet):
