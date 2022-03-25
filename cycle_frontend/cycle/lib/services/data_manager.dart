12ebe3d7378d67b1c2522ceb24a609d7011c528a
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2022-03-25 11:51:21 +0000

    Implement navigation by splitting route up into walking and cycling subroutes

--- cycle_frontend/cycle/lib/services/data_manager.dart
+++ cycle_frontend/cycle/lib/services/data_manager.dart
@@ -2,10 +2,12 @@ import 'package:cycle/models/docking_station.dart';
 import 'package:cycle/models/landmark.dart';
 import 'package:cycle/models/stop.dart';
 import 'package:cycle/services/network_helper.dart';
+import 'package:latlong2/latlong.dart';
 
 const String kBackEndUrl = 'https://agile-citadel-13372.herokuapp.com/';
 
 const String kBikePointsPath = 'bikepoints/?format=json';
+const String kClosestBikePointPath = 'closest/bikepoint';
 const String kLandmarksPath = 'landmarks/?format=json';
 
 Future<List<DockingStation>> getDockingStations() async {
@@ -17,6 +19,33 @@ Future<List<DockingStation>> getDockingStations() async {
           .toList();
 
   return dockingStations;
+}
+
+Future<DockingStation> getClosestDockingStationWithBikesAvailable(
+    LatLng latLng, int numberOfBikes) async {
+  NetworkHelper networkHelper = NetworkHelper(kBackEndUrl +
+      kClosestBikePointPath +
+      "/with/$numberOfBikes/bikes/from/${latLng.latitude}/${latLng.longitude}");
+
+  print(kBackEndUrl +
+      kClosestBikePointPath +
+      "/with/$numberOfBikes/bikes/from/${latLng.latitude}/${latLng.longitude}");
+  DockingStation dockingStation =
+      DockingStation.fromJson(await networkHelper.getData());
+
+  return dockingStation;
+}
+
+Future<DockingStation> getClosestDockingStationWithEmptyDocks(
+    LatLng latLng, int numberOfBikes) async {
+  NetworkHelper networkHelper = NetworkHelper(kBackEndUrl +
+      kClosestBikePointPath +
+      "/with/$numberOfBikes/empty_docks/from/${latLng.latitude}/${latLng.longitude}");
+
+  DockingStation dockingStation =
+      DockingStation.fromJson(await networkHelper.getData());
+
+  return dockingStation;
 }
 
 Future<List<Landmark>> getLandmarks() async {
--- cycle_frontend/cycle/lib/services/navigation.dart
+++ cycle_frontend/cycle/lib/services/navigation.dart
@@ -1,4 +1,7 @@
 import 'dart:async';
+import 'package:cycle/models/docking_station.dart';
+import 'package:cycle/models/stop.dart';
+import 'package:cycle/services/data_manager.dart';
 import 'package:flutter_mapbox_navigation/library.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -8,18 +11,33 @@ class Navigation {
     LatLng(51.504501, -0.086500)
   ];
 
-  final _home =
-      WayPoint(name: "Home", latitude: 51.458060, longitude: -0.371500);
-
-  final _store =
-      WayPoint(name: "Store", latitude: 51.469613, longitude: -0.357147);
+  final int numberOfRiders = 3;
 
   late MapBoxNavigation _directions;
-  late MapBoxOptions _options;
+  late MapBoxOptions _walkingOptions;
+  late MapBoxOptions _cyclingOptions;
 
   Navigation() {
     _directions = MapBoxNavigation(onRouteEvent: _onEmbeddedRouteEvent);
-    _options = MapBoxOptions(
+
+    _walkingOptions = MapBoxOptions(
+        //initialLatitude: 36.1175275,
+        //initialLongitude: -115.1839524,
+        zoom: 19.0,
+        tilt: 0.0,
+        bearing: 0.0,
+        enableRefresh: true,
+        alternatives: true,
+        voiceInstructionsEnabled: true,
+        bannerInstructionsEnabled: true,
+        allowsUTurnAtWayPoints: true,
+        mode: MapBoxNavigationMode.walking,
+        units: VoiceUnits.imperial,
+        simulateRoute: true, //uncomment for debugging
+        animateBuildRoute: true,
+        language: "en");
+
+    _cyclingOptions = MapBoxOptions(
         //initialLatitude: 36.1175275,
         //initialLongitude: -115.1839524,
         zoom: 19.0,
@@ -38,10 +56,52 @@ class Navigation {
   }
 
   void navigate() async {
+    for (int i = 0; i < _stops.length; i++) {
+      await buildRouteBetweenTwoPoints(
+          _stops[i], _stops[i + 1], numberOfRiders);
+    }
+  }
+
+  Future<void> buildRouteBetweenTwoPoints(
+      LatLng start, LatLng stop, int numberOfRiders) async {
+    DockingStation startDockingStation =
+        await getClosestDockingStationWithBikesAvailable(start, numberOfRiders);
+    DockingStation stopDockingStation =
+        await getClosestDockingStationWithEmptyDocks(stop, numberOfRiders);
+
+    await buildWalkingRouteBetweenTwoPoints(
+        start, LatLng(startDockingStation.lat, startDockingStation.lon));
+
+    await buildCyclingRouteBetweenTwoPoints(
+        LatLng(startDockingStation.lat, startDockingStation.lon),
+        LatLng(stopDockingStation.lat, stopDockingStation.lon));
+
+    await buildWalkingRouteBetweenTwoPoints(
+        LatLng(stopDockingStation.lat, stopDockingStation.lon), stop);
+  }
+
+  Future<void> buildWalkingRouteBetweenTwoPoints(
+      LatLng start, LatLng stop) async {
     var wayPoints = <WayPoint>[];
-    wayPoints.add(_home);
-    wayPoints.add(_store);
-    await _directions.startNavigation(wayPoints: wayPoints, options: _options);
+    wayPoints.add(WayPoint(
+        name: "Start", latitude: start.latitude, longitude: start.latitude));
+    wayPoints.add(WayPoint(
+        name: "Stop", latitude: stop.latitude, longitude: stop.latitude));
+
+    await _directions.startNavigation(
+        wayPoints: wayPoints, options: _walkingOptions);
+  }
+
+  Future<void> buildCyclingRouteBetweenTwoPoints(
+      LatLng start, LatLng stop) async {
+    var wayPoints = <WayPoint>[];
+    wayPoints.add(WayPoint(
+        name: "Start", latitude: start.latitude, longitude: start.latitude));
+    wayPoints.add(WayPoint(
+        name: "Stop", latitude: stop.latitude, longitude: stop.latitude));
+
+    await _directions.startNavigation(
+        wayPoints: wayPoints, options: _cyclingOptions);
   }
 
   Future<void> _onEmbeddedRouteEvent(RouteEvent e) async {
