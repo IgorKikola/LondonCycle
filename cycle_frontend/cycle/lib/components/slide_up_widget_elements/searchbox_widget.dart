825d3cf2736b383e6916e5e2d418880de19ae55c
Mikolaj Gierak <gierak.mikolaj@gmail.com>
2022-04-02 17:14:57 +0200

    Extract widgets into separate files.

---
+++ cycle_frontend/cycle/lib/components/slide_up_widget_elements/searchbox_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchBoxWidget extends StatelessWidget {
+  const SearchBoxWidget({
+    Key? key,
+    required this.searchBar,
+  }) : super(key: key);
+
+  final Widget searchBar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      width: 300,
+      decoration: BoxDecoration(
+        color: Colors.lightBlue[200],
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: searchBar,
+    );
+  }
+}
