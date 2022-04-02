6e416620af6c2ca91adc5e3cabd35c798b606be0
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2022-04-02 20:41:38 +0300

    change mapbox api key

--- cycle_frontend/cycle/lib/utilities/map_box_config.dart
+++ cycle_frontend/cycle/lib/utilities/map_box_config.dart
@@ -1,9 +1,9 @@
 class MapBoxConfig {
   static const String mapBoxURL = 'api.mapbox.com';
   static const String mapBoxAPIKey =
-      'pk.eyJ1IjoibWFyaWFuZ2FydHUiLCJhIjoiY2t6aWh3Yjg1MjZmNTJ1bzZudjQ3NW45NSJ9.LJQ8MpEySa-SINNUc8z9rQ';
+      'pk.eyJ1IjoiZ2FydHVtYXJpYW4iLCJhIjoiY2wxaTRic2hjMGZzcjNjbWoyejN0eXNpbCJ9.mmBmmHl0m43vyKqH3vw-hg';
   static const String mapBoxMapAPI =
-      'https://$mapBoxURL/styles/v1/mariangartu/ckzjt4a9d000v14s451ltur5q/tiles/256/{z}/{x}/{y}@2x?access_token=$mapBoxAPIKey';
+      'https://$mapBoxURL/styles/v1/gartumarian/cl1i4i6xa002e15o93ktpssyh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2FydHVtYXJpYW4iLCJhIjoiY2wxaTRic2hjMGZzcjNjbWoyejN0eXNpbCJ9.mmBmmHl0m43vyKqH3vw-hg';
   static const String mapBoxRoutingAPI = '/directions/v5/mapbox/cycling/';
   static const String mapBoxGeocodingAPI = '/geocoding/v5/mapbox.places/';
 }
