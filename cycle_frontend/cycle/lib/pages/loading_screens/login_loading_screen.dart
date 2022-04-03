46128dbe2a49ace744563953c9e3d326450df941
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2022-04-03 15:29:28 +0300

    remove unused import in login_loading_screen

--- cycle_frontend/cycle/lib/pages/loading_screens/login_loading_screen.dart
+++ cycle_frontend/cycle/lib/pages/loading_screens/login_loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:cycle/models/login_request_model.dart';
-import 'package:cycle/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
